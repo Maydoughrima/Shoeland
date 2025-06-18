b231a923c6f17d100e421b6f5b69027c96b2992e
John Luis Nathaniel Mercado <johnluismercado863@gmail.com>
2025-06-18 13:24:28 +0800

    Added 2 new sections with full responsive

--- javas.js
+++ javas.js
@@ -4,3 +4,17 @@ const mobileMenu = document.getElementById('mobile-menu')
 hamburger.addEventListener('click',()=>{
     mobileMenu.classList.toggle('active');
 });
+
+  const ctaButtons = document.querySelectorAll('.ctaU, .ctaM, .ctaW, .ctaK');
+
+  ctaButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Remove 'active' class from all
+      ctaButtons.forEach(btn => btn.classList.remove('active'));
+
+      // Add 'active' to the clicked one
+      button.classList.add('active');
+    });
+  });
+
+  
